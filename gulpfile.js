58508d63e9a02ea7f54f18b90bf2470240edba67
Irina <irenquick@gmail.com>
2022-09-21 15:16:24 +0300

    Добавляет спрайты

--- gulpfile.js
+++ gulpfile.js
@@ -65,13 +65,13 @@ const createWebp = () => {
 
 // SVG
 const svg = () =>
-gulp.src(['source/img/*.svg', '!source/img/icons/*.svg'])
+gulp.src(['source/img/**/*.svg', '!source/img/sprites/*.svg'])
 .pipe(svgo())
 .pipe(gulp.dest('build/img'))
 
 
 const sprite = () => {
-  return gulp.src('source/img/icons/*.svg')
+  return gulp.src('source/img/sprites/*.svg')
   .pipe(svgo())
   .pipe(svgstore({
     inlinesvg: true
